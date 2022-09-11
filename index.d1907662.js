87f711f26b07c88cfcb67658c4b93e1b5fcb8df2
Denys07748 <95140883+Denys07748@users.noreply.github.com>
2022-09-11 16:11:03 +0000

    Deploying to gh-pages from @ Denys07748/Codewars@e6756e57bdadbdca0369f9ccb0899c87538ccae9 🚀

---
+++ index.d1907662.js
@@ -0,0 +1,2 @@
+function o(o){let c=o.split("");if(c.includes("#")){const o=c.indexOf("#");c=c.slice(0,o)}return c.join("")}console.log(function(o){const c=[];for(let e=1;e<o;e+=1)e%2!=0&&c.push(e);return c.length}(7)),console.log(o("www.codewars.com#about")),console.log(o("www.codewars.com/katas/?page=1#about")),console.log(o("www.codewars.com/katas/"));
+//# sourceMappingURL=index.d1907662.js.map
