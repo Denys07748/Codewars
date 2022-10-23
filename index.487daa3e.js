4c47e8232614073d5d99dd3ea735745788b9c7e3
Denys07748 <95140883+Denys07748@users.noreply.github.com>
2022-10-23 21:12:50 +0000

    Deploying to gh-pages from @ Denys07748/Codewars@6ec2e65d87228049bfd69d9019144ec8a61f898f 🚀

---
+++ index.487daa3e.js
@@ -0,0 +1,2 @@
+console.log([[0,1,2,3,45],[10,11,12,13,14],[20,21,22,23,24],[30,31,32,33,34]].flatMap((function(n){return n+""})).join("\n")),console.log(function(n){for(var o=n.sort((function(n,o){return n-o})),r=0,t=0;t<o.length-1;t++)r+=o[t+1]-o[t];return r}([-13,4,-19,5,15,-12,-20,1,-13,-10,-7,-9,-5]));
+//# sourceMappingURL=index.487daa3e.js.map
